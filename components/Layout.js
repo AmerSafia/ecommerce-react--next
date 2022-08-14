e589880b79752160af0571c7038553fbd436f7c9
MohammadSafia <amer.safia321@gmail.com>
2022-08-14 20:56:12 +0300

    updated

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
-import Navbar from "./Navbar.jsx";
-import Footer from "./Footer.jsx";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
